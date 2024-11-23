8f32f90b063af3accd8a89cfccb29465be01fd6c
catalin <cdojamihai@gmail.com>
2024-11-23 15:25:42 +0100

    installing extra modules and updating skeleton

--- backend/src/controllers/entries.controller.js
+++ backend/src/controllers/entries.controller.js
@@ -1,4 +1,4 @@
-import {pool} from '../db.js'
+//import {pool} from '../db.js'
 
 /**
  * Recovers the entries from the database
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,6 @@
 import app from './app.js'
 import { PORT } from './config.js'
 import express from "express";
-import cookieParser from "cookie-parser";
-import multer from "multer";
 
 // Initializes the server based on the information in app.js and config.js
 app.listen(PORT, () => {
